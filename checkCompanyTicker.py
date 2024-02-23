a43838e80be18ba30fbf771098bd27586235d7e3
amirmahdi-alavi <sam.alavi@hotmail.com>
2024-02-23 16:43:29 -0500

    added check for company code

--- checkCompanyTicker.py
+++ checkCompanyTicker.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-
+import time
+import os
 
 '''
 1- Ticker.TO check for Toronto and has price
@@ -28,7 +29,46 @@ def request_company_page(ticker, toronto=True):
 def check_company(response, toronto=True):
     parser = BeautifulSoup(response.content, 'html.parser')
     location = parser.find('div', attrs={'class': 'C($tertiaryColor) Fz(12px)'}).find('span').text.split('-')[0].strip()
+    regular_price = parser.find('fin-streamer', attrs={'class': 'Fw(b) Fz(36px) Mb(-4px) D(ib)'}).text
     cond = False
     if toronto:
         city = "Toronto"
+    else:
+        city = 'TSXV'
     
+    if location == city and float(regular_price) > 0:
+        cond = True
+
+    return cond
+
+os.makedirs('data', exist_ok=True)
+
+list_t = list()
+list_v = list()
+
+for idx, row in df.iterrows():
+    company_name = row['Name']
+    ticker = row['Root\nTicker']
+    t_response = request_company_page(ticker)
+    v_response = request_company_page(ticker, toronto=False)
+    t_cond = check_company(t_response)
+    v_cond = check_company(v_response, toronto=False)
+    
+    if t_cond:
+        list_t.append({
+            'CompanyName': company_name,
+            'Ticker': ticker+'.TO'
+        })
+    if v_cond:
+        list_v.append({
+            'CompanyName': company_name,
+            'Ticker': ticker+'.V'
+        })
+
+    if idx % 10 == 0:
+        df_to = pd.DataFrame(list_t)
+        df_to.to_csv('TorontoCompanies.csv')
+
+        df_v = pd.DataFrame(list_v)
+        df_v.to_csv('VancouverCompanies.csv')
+
