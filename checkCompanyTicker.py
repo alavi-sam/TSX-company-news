a9e9059a8f1fd09c20648cab45af6cfd8493dbd6
amirmahdi-alavi <sam.alavi@hotmail.com>
2024-02-28 18:05:25 -0500

    checked companies csv

--- checkCompanyTicker.py
+++ checkCompanyTicker.py
@@ -3,6 +3,9 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import os
+import random
+from glob import glob
+import json
 
 '''
 1- Ticker.TO check for Toronto and has price
@@ -13,16 +16,39 @@ import os
 
 
 
+class Request:
+    DIRS = glob(os.path.join('UaJson', '*.json')) 
 
-df = pd.read_csv('TSX.csv')
+    def _select_ua(self):
+        ua_file = random.choice(self.DIRS)
+        with open(ua_file, 'r') as f:
+            ua = random.choice(json.load(f))['ua']
+            while 'Android' in ua or 'iPhone' in ua or 'Tablet' in ua:
+                with open(ua_file, 'r') as f:
+                    ua = random.choice(json.load(f))['ua']
+        return ua
+
+    def get(self, url):
+        # ua = self._select_ua()
+        ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
+        return requests.get(url, headers={'User-Agent': ua})
+
+
+custom_request = Request()
+
+
+df = pd.read_csv('TSXV.csv')
 
 def request_company_page(ticker, toronto=True):
+    # if ticker == 'VGZ':
+    #     print()
+
     if toronto:
         ticker += '.TO'
     else:
         ticker += '.V'
 
-    response = requests.get(f'https://ca.finance.yahoo.com/quote/{ticker}?.tsrc=fin-srch')
+    response = custom_request.get(f'https://ca.finance.yahoo.com/quote/{ticker}')
     return response
 
 
@@ -41,34 +67,69 @@ def check_company(response, toronto=True):
 
     return cond
 
-os.makedirs('data', exist_ok=True)
+if __name__ == '__main__':
 
-list_t = list()
-list_v = list()
 
-for idx, row in df.iterrows():
-    company_name = row['Name']
-    ticker = row['Root\nTicker']
-    t_response = request_company_page(ticker)
-    v_response = request_company_page(ticker, toronto=False)
-    t_cond = check_company(t_response)
-    v_cond = check_company(v_response, toronto=False)
-    
-    if t_cond:
-        list_t.append({
-            'CompanyName': company_name,
-            'Ticker': ticker+'.TO'
-        })
-    if v_cond:
-        list_v.append({
-            'CompanyName': company_name,
-            'Ticker': ticker+'.V'
-        })
+    os.makedirs('data-tsxv', exist_ok=True)
 
-    if idx % 10 == 0:
+    list_t = list()
+    list_v = list()
+    list_error = list()
+
+    for idx, row in df.iterrows():
+        company_name = row['Name']
+        ticker = row[' Root\nTicker ']
+        t_response = request_company_page(ticker)
+        v_response = request_company_page(ticker, toronto=False)
+        # try:
+        try:
+            t_cond = check_company(t_response)
+        except:
+            t_cond = False
+            try:
+                v_cond = check_company(v_response, toronto=False)
+            except:
+                v_cond = False
+
+            # print(f'errorrrrr -------- \n https://ca.finance.yahoo.com/quote/{ticker}.TO?.tsrc=fin-srch')
+
+        # try:
+        # v_cond = check_company(v_response, toronto=False)
+        # except:
+        # t_cond = False
+        # print(f'errorrrrr -------- \n https://ca.finance.yahoo.com/quote/{ticker}.V?.tsrc=fin-srch')
+        
+        if t_cond:
+            list_t.append({
+                'CompanyName': company_name,
+                'Ticker': ticker+'.TO'
+            })
+            print('found {}.TO'.format(ticker))
+        else:
+            if v_cond:
+                list_v.append({
+                    'CompanyName': company_name,
+                    'Ticker': ticker+'.V'
+                })
+                print('found {}.V'.format(ticker))
+
+            else:
+                # check_company(ticker)
+                # check_company(ticker, toronto=False)
+                # print('ERRORRRRRR  ', ticker)
+                list_error.append({
+                    'CompanyName': company_name,
+                    'Ticker': ticker
+                })
+
         df_to = pd.DataFrame(list_t)
-        df_to.to_csv(os.path.join('data', 'TorontoCompanies.csv'))
+        df_to.to_csv(os.path.join('data-tsxv', 'TorontoCompanies.csv'))
 
         df_v = pd.DataFrame(list_v)
-        df_v.to_csv(os.path.join('data', 'VancouverCompanies.csv'))
+        df_v.to_csv(os.path.join('data-tsxv', 'VancouverCompanies.csv'))
+
+        df_error = pd.DataFrame(list_error)
+        df_error.to_csv(os.path.join('data-tsxv', 'error.csv'))
+        if idx % 10 == 0:
+            print(idx, ' searched')
 
